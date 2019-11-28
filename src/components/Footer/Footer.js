c935dc25541869802bdd93f4906a44287d7a421c
Axel <axosar@gmail.com>
2019-11-28 10:46:13 -0600

    Detalles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,8 @@ class Footer extends Component {
     return (
       <footer className="footer">
         <div className="container">
-          <p className="text-center">@ 2019 | Todos Los Derechos Reservados.<br/><Link to="http://www.imcyc.com">Instituto Mexicano del Cemento y del Concreto A.C.</Link></p>
+          <h4>COMPARTIR EL CONOCIMIENTO</h4>
+          <p className="text-center"><Link to="http://www.imcyc.com">Instituto Mexicano del Cemento y del Concreto A.C.</Link><br/>® 2019 | Todos Los Derechos Reservados.<br/></p>
           <ul className="social_footer_ul">
             <li><Link to="/"><i className="fa fa-facebook-f"></i></Link></li>
             <li><Link to="/"><i className="fa fa-twitter"></i></Link></li>
