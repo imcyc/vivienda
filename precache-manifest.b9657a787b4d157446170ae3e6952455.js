868ad5c929f31911190bdae1cf19320713467bff
Axel <axosar@gmail.com>
2019-11-27 18:00:47 -0600

    Updates

---
+++ precache-manifest.b9657a787b4d157446170ae3e6952455.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba0efa8d2a48ad33d7ee7a030e62546",
+    "url": "/vivienda/index.html"
+  },
+  {
+    "revision": "a2cc2db705e24e56157f",
+    "url": "/vivienda/static/css/2.7116a6b8.chunk.css"
+  },
+  {
+    "revision": "fe6a43f0d3f101977ce1",
+    "url": "/vivienda/static/css/main.7ff0589a.chunk.css"
+  },
+  {
+    "revision": "a2cc2db705e24e56157f",
+    "url": "/vivienda/static/js/2.ebfa0b4f.chunk.js"
+  },
+  {
+    "revision": "fe6a43f0d3f101977ce1",
+    "url": "/vivienda/static/js/main.b7275c6b.chunk.js"
+  },
+  {
+    "revision": "2d76689bfb6657b09f10",
+    "url": "/vivienda/static/js/runtime-main.8adad268.js"
+  },
+  {
+    "revision": "3314437c1d99632c63d31e58ea886f19",
+    "url": "/vivienda/static/media/logo-habitat.3314437c.png"
+  },
+  {
+    "revision": "15ca9bfd1b968a1f06ec9ddc79032d46",
+    "url": "/vivienda/static/media/logo-imcyc.15ca9bfd.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vivienda/precache-manifest.b9657a787b4d157446170ae3e6952455.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vivienda/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
