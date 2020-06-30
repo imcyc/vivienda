84e849d4d4722087b99fabcd5b299746f1af8124
Axel <axosar@gmail.com>
2020-06-30 14:23:50 -0500

    contrasena

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: "140220",
-      password: "@140220"
+      user: "ruribe@imcyc.com",
+      password: "ruribe@"
     }
   }
 
